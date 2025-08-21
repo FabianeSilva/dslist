d356148d40b4a21af53fee7cb2bbe5134d9b3da1
Fabi <fabiane1974@hotmail.com>
2025-08-21 18:26:56 -0300

    GET game by id

--- src/main/java/com/fabianesilva/dslist/controllers/GameController.java
+++ src/main/java/com/fabianesilva/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.fabianesilva.dslist.controllers;
 
+import com.fabianesilva.dslist.dto.GameDTO;
 import com.fabianesilva.dslist.dto.GameMinDTO;
 import com.fabianesilva.dslist.entities.Game;
 import com.fabianesilva.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/fabianesilva/dslist/services/GameService.java
+++ src/main/java/com/fabianesilva/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.fabianesilva.dslist.services;
 
+import com.fabianesilva.dslist.dto.GameDTO;
 import com.fabianesilva.dslist.dto.GameMinDTO;
 import com.fabianesilva.dslist.entities.Game;
 import com.fabianesilva.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,21 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    //Esse @Transactional é para seguir as boas práticas, para colocar nos métodos do Service para que ele
+    //fique transacional, para que ele garanta que a operaçao vai acontecer, obedecendo aos principios das
+    //transações ASID, atomica, consistente, isolada e duravel
+    //é utilizado o argumento readOnly=true porque nesse caso estamos assegurando que não faremos nenhuma
+    //operação de escrita, então não vamos bloquear o banco de dados para escrita, então vai ficar mais rapido
+    //dessa forma
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
