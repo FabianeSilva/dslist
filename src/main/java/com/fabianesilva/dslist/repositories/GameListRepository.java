1d89143f91a4ae94a111c1e9c41bae42b4fbe401
Fabi <fabiane1974@hotmail.com>
2025-08-24 13:35:13 -0300

    Move games on list

--- src/main/java/com/fabianesilva/dslist/repositories/GameListRepository.java
+++ src/main/java/com/fabianesilva/dslist/repositories/GameListRepository.java
@@ -3,6 +3,14 @@ package com.fabianesilva.dslist.repositories;
 import com.fabianesilva.dslist.entities.Game;
 import com.fabianesilva.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+    //Quando é uma função que não seja o SELECT, seja UPDATA, DELETE ou INSERT, temos que colocar o @Modifying
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void uptadeBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/com/fabianesilva/dslist/services/GameListService.java
+++ src/main/java/com/fabianesilva/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.fabianesilva.dslist.dto.GameListDTO;
 import com.fabianesilva.dslist.dto.GameMinDTO;
 import com.fabianesilva.dslist.entities.Game;
 import com.fabianesilva.dslist.entities.GameList;
+import com.fabianesilva.dslist.projections.GameMinProjection;
 import com.fabianesilva.dslist.repositories.GameListRepository;
 import com.fabianesilva.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     //Esse @Transactional é para seguir as boas práticas, para colocar nos métodos do Service para que ele
     //fique transacional, para que ele garanta que a operaçao vai acontecer, obedecendo aos principios das
     //transações ASID, atomica, consistente, isolada e duravel
@@ -30,5 +34,22 @@ public class GameListService {
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        //a linha de comando abaixo é um ternário, onde ? significa que se o source for menor que o destination então
+        //e o : é o else, senão, caso contrário o menor será o destination
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.uptadeBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
